9910764b40c4c2e46590410cbeed1e1f1c135a57
meralkilicgil <meralkilicgil@gmail.com>
2024-03-03 13:02:42 +0300

    initial commit

---
+++ src/main/java/com/meri/murdermysterygame/MurderMysteryGameApplication.java
@@ -0,0 +1,13 @@
+package com.meri.murdermysterygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurderMysteryGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MurderMysteryGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meri/murdermysterygame/MurderMysteryGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meri.murdermysterygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurderMysteryGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
